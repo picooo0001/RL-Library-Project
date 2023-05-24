d13adde1618491e3e5b3ee3027ee7527848f08d5
Nico Peuser <76703205+picooo0001@users.noreply.github.com>
2023-05-24 19:32:38 +0200

    Add files via upload

--- rllib/artists.py
+++ rllib/artists.py
@@ -1,44 +1,97 @@
 """"Modelling the artists of the festival."""
 
-import json
+import csv
 
 class Artists:
     """Class of the artists who perform at Rolling Loud Munich
     
-    The data of the artists is saved in `data.json`"""
+    The data of the artists is saved in `data.csv`"""
     
-    def __init__(self, name, headliner, stage, time, date): #name, headliner, etc, useful? notwendig
-        """Reads the file `data.json` 
-        and checks if every arg has its values (one for every artist)
+    def __init__(self):
+        """Checks if the .csv file is accessable and instaziation of the args
         
         Args:
             name: name of the artist
             headliner: is the artist headliner or not (boolean)
             stage: main stage or secondary stage
-            time: the timespan for every artist on stage 
-            date: DD/MM/YYYY of when the artist is on stage"""
+            date: DD.MM.YYYY of when the artist is on stage
+            time: the timespan for every artist on stage """
+    
         
-        try:
-            f = open("rllib/data.json")
-            data = json.load(f)
+        try: #checks if the file is openable
+            file_path = "rllib/data.csv"
+            f = open(file_path, "r")
+            reader = csv.reader(f)
         except:
-            raise RuntimeError("The `data.json` file could not been opened. Please check!")
-            
-        names = (artist["name"] for artist in data["artists"])
-        stage = (artist["stage"] for artist in data["artists"])
-        time = (artist["time"] for artist in data["artists"])
-        date = (artist["date"] for artist in data["artists"])
-
-        if all(names) & all(headliner) & all(time) & all(date):
+            raise RuntimeError("The data.csv file could not been opened. Please check!")
+                           
+        next(reader) #header line will be skipped, that only the important data will be written in file
+        self.artists = tuple(artists[0] for artists in reader)
+        f.seek(0) #resets the "read" state of the file
+        next(reader)
+        self.headliner = tuple(headliner[1] for headliner in reader)
+        f.seek(0)
+        next(reader)
+        self.stage = tuple(stage[2] for stage in reader)
+        f.seek(0)
+        next(reader)
+        self.date = tuple(date[3] for date in reader)
+        f.seek(0)
+        next(reader)
+        self.time = tuple(time[4] for time in reader)
+        f.seek(0)
+        next(reader)
+        
+    def check_rows(self):
+        """ Checks if all rows are complete. If this is not the case, a RuntimeError will appear.
+        The number 59 is pre defined. """
+        
+        #only artists collum will be checked, because all collums have the same length
+        if len(self.artists) == 59:
             pass
         else:
-            raise RuntimeError("There are missing values in `names` section. Please check!")
+            raise RuntimeError("Missing collum. Please check!")
             
-        #To-Do check "Headliner" Values
+    
+    def search_artist(self):
+        """Searchs for an artist by user input. It takes the data from the tuple above.
+        Input: name of the artist (typecasting: all together all lowercase)
+        Output: name, headliner, stage, time, date
+        """
+        
+        user_input = input("Search: ") #requests input from user
+        user_input = user_input.replace(" ", "").lower() #typecasting
+        found = False 
+    
+        for artist in self.artists: #loops over the tuple artists
+            formatted_artist = artist.replace(" ", "").lower() #typecasting
+            if user_input in formatted_artist:
+                index = formatted_artist.index(user_input) #find index to search in the other tuples
+                headliner = self.headliner[index]
+                if headliner == "true": #not necessary
+                    headliner = "is headliner"
+                else:
+                    headliner = "no headliner"
+                stage = self.stage[index]
+                time = self.time[index]
+                date = self.date[index]
+                output = ", ".join([artist, stage, time, date]) #print the output in one line
+                print(output)
+                found = True #variable form above to true, function ends
 
-        f.close()
+        if not found: #= if found != true
+            print("Sorry! We couldn't find what you were looking for!")
             
-            
+    
+        
+
+
+
+
+
+
+
+
         
 
         
